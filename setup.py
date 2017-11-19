08298b2e50b22145068c4d29278b06e34f6e238a
Ashish Pandey <ap3666@nyu.edu>
2017-11-19 13:15:48 -0500

    Update setup.py
    changed directory

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1.0',
     author='Ashish Pandey',
     author_email='ashish.jnvm@gmail.com',
-    packages=['ProjectNeeds','ProjectNeeds.Database','ProjectNeeds.Blockchain'],
+    packages=['ProjectNeeds','ProjectNeeds.database','ProjectNeeds.Blockchain'],
     description='Useful things necessary for projects',
 )
