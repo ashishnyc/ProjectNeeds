b869a51f1d1851b579603d618805aa14aeb77625
ashishpandey <ashish.jnvm@gmail.com>
2017-12-16 03:02:16 -0500

    added feature of not sending select query

--- ProjectNeeds/database/MySqlConnection.py
+++ ProjectNeeds/database/MySqlConnection.py
@@ -1,55 +1,84 @@
 #!/usr/bin/python
 import MySQLdb as mdb
-import ConfigParser, os, sys
+import ConfigParser
+import os
+import sys
+import pandas as pd
+
 '''
     Database connection class
 '''
-dbConn=None
+dbConn = None
+
 
 class MySqlConnection:
-    def __init__(self, ConfigFile):
+    def __init__(self, config_file):
         self.__config = ConfigParser.RawConfigParser()
-        self.__ConfigFile = ConfigFile
-        if not os.path.isfile(self.__ConfigFile):
-            print('ConfigFile[{0}] does not exist. Exiting.'.format(self.__ConfigFile))
+        if not os.path.isfile(config_file):
+            print('config file[{file}] does not exist. Exiting.'.format(file=config_file))
             sys.exit(0)
-        self.__config.read(self.__ConfigFile)
+        self.__config.read(config_file)
         self.__host = self.__config.get('DATABASE', 'HOST')
         self.__port = self.__config.get('DATABASE', 'PORT')
         self.__username = self.__config.get('DATABASE', 'USERNAME')
         self.__password = self.__config.get('DATABASE', 'PASSWORD')
         self.__database = self.__config.get('DATABASE', 'DATABASE')
+        query = """SELECT TABLE_NAME,COLUMN_NAME,DATA_TYPE 
+                   FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='{schema}'""".\
+            format(schema=self.__database)
+        try:
+            self.__open()
+            self.__db_structure = pd.read_sql(query, con=self.__dbconn)
+            self.__close()
+        except mdb.Error, e:
+            raise e
 
     def __open(self):
-        try:
-            self.__dbconn = mdb.Connection(self.__host, self.__username, self.__password, self.__database)
-            self.__cursor = self.__dbconn.cursor()
-        except mdb.Error as e:
-            print(str(e))
+        self.__dbconn = mdb.Connection(self.__host, self.__username, self.__password, self.__database)
+        self.__cursor = self.__dbconn.cursor()
 
     def __close(self):
         self.__cursor.close()
         self.__dbconn.close()
 
-    def getoneresult(self, query):
-        try:
-            self.__open()
-            self.__cursor.execute(query)
-            result = self.__cursor.fetchone()
-            self.__close()
-        except Exception as e:
-            print(str(e))
-        return result
+    def __get_column_datatype(self, table_name, column_name):
+        return (self.__db_structure[(self.__db_structure['TABLE_NAME'] == table_name)
+                                   & (self.__db_structure['COLUMN_NAME'] == column_name)]["DATA_TYPE"].values[0])
 
-    def getmultipleresults(self, query):
+    def __create_select_query(self, table_name, column_names, value_filters=None, like_filters=None):
+        query = "SELECT "
+        query += ','.join(column_names)
+        query += " FROM "
+        query += table_name
+        if value_filters is not None:
+            query += " WHERE "
+            query += ' AND '.join(["{col}{operator}{value}".
+                                  format(col=k
+                                         , operator=v[0]
+                                         , value="'{0}'".format(v[1]) if self.__get_column_datatype(table_name=table_name
+                                                                                                    , column_name=k) == 'varchar' else v[1])
+                                   for k, v in value_filters.iteritems()]
+                                  )
+
+        if like_filters is not None:
+            if value_filters is not None:
+                query += " AND "
+            else:
+                query += " WHERE "
+            query += ' AND '.join(["{col} LIKE '{value}'".format(col=k, value=v) for k, v in like_filters.iteritems()])
+        return query
+
+    def get_values(self, table_name, column_names, value_filters=None, like_filters=None):
         try:
             self.__open()
-            self.__cursor.execute(query)
-            result = self.__cursor.fetchall()
+            df = pd.read_sql(self.__create_select_query(table_name=table_name
+                                                        , column_names=column_names
+                                                        , value_filters=value_filters
+                                                        , like_filters=like_filters), con=self.__dbconn)
             self.__close()
-        except Exception as e:
-            print(str(e))
-        return result
+        except mdb.Error, e:
+            raise e
+        return df
 
     def insertrecord(self, query):
         try:
@@ -70,5 +99,3 @@ class MySqlConnection:
         except Exception as e:
             print(str(e))
 
-    def get_conn(self):
-        return mdb.Connection(self.__host, self.__username, self.__password, self.__database)
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='ProjectNeeds',
-    version='0.1.0',
+    version='0.2.0',
     author='Ashish Pandey',
     author_email='ashish.jnvm@gmail.com',
-    packages=['ProjectNeeds','ProjectNeeds.database','ProjectNeeds.Blockchain'],
+    packages=['ProjectNeeds', 'ProjectNeeds.database', 'ProjectNeeds.Blockchain'],
     description='Useful things necessary for projects',
 )
